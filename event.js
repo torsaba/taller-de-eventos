bedff106230c0d248c3ea6a867bbd18d74289759
torsaba <lucia.bperez.a@gmail.com>
2023-08-15 23:19:45 -0300

    Solucion-click

--- event.js
+++ event.js
@@ -1,8 +1,14 @@
 const div = document.querySelector("div");
+const boton = document.querySelector("button");
 
 div.style.background = "Green";
 div.style.padding = "5px";
 
+const saludo = () => {
+    alert("Hola!");
+}
+boton.addEventListener("click", saludo);
+boton.removeEventListener("click", saludo);
 
 function saludar() {
     alert("Hola! Soy el div.");
